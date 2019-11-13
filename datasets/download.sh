883016447635528784cd28f6906b87e5bd7f930a
Riley Kopp <koppril7@gmail.com>
2019-11-13 11:01:27 -0700

    modified download script to only load a day

--- datasets/download.sh
+++ datasets/download.sh
@@ -1,9 +1,10 @@
-for i in $(seq -w 2 90);
-do wget -c https://csr.lanl.gov/data-fence/1573636132/ZhvWw6Qvs6-nTpH7z1UQHJx3fzw=/unified-host-network-dataset-2017/netflow/netflow_day-$i.bz2;
-bzip2 -dk netflow_day-0$i.bz2;
-done
+echo "This will take a long time, are you sure? [y/n]"
+read input
 
-
-python3 tranlasteNetworkData.py
-
+if [$input -eq "y"]
+then
+    wget -c https://csr.lanl.gov/data-fence/1573636132/ZhvWw6Qvs6-nTpH7z1UQHJx3fzw=/unified-host-network-dataset-2017/netflow/netflow_day-2.bz2;
+    bzip2 -dk netflow_day-02.bz2;
+    python3 tranlasteNetworkData.py
+fi
 
