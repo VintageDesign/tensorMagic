6e0de83afb2453162875784922a29416af5953e0
Riley Kopp <koppril7@gmail.com>
2019-11-12 23:10:36 -0700

    Changing to a bigger computer

---
+++ datasets/download.sh
@@ -0,0 +1,1 @@
+for i in $(seq -w 2 90); do wget -c https://csr.lanl.gov/data-fence/1573636132/ZhvWw6Qvs6-nTpH7z1UQHJx3fzw=/unified-host-network-dataset-2017/netflow/netflow_day-$i.bz2; done
--- datasets/translateNetworkData.py
+++ datasets/translateNetworkData.py
@@ -1,6 +1,43 @@
-import igraph
+import csv
+import time
 
-def getData(day=3, datadir="./Flow", outdir=Null):
-    x = 0
+print("Opening CSV File...")
+start = time.time()
+csvFile = open('netflow_day-02')
+day = 2 # Hard coded for netflow-day-2
+hour = 11 # we only want an hours worth of data cause RAM would fill up... probably will anyway
 
-    flows =
+print("Translating Data...")
+
+
+fieldNames = ["Time", "Duration", "SrcDevice", "DstDevice", "Protocol", "SrcPort", "DstPort", "SrcPackets", "DstPackets", "SrcBytes", "DstBytes"]
+
+dictionaries = csv.DictReader(csvFile, fieldnames=fieldNames)
+middleData = []
+finalData = []
+
+for row in dictionaries:
+    if  row['Protocol'] == '6':
+        row["Time"] = int(row["Time"]) - 3600*24*(day - 1)/3600
+        middleData.append(row)
+
+
+print("Outputting Final Data...")
+f = open("Out.txt", "w+")
+
+for row in middleData:
+    if row["Time"] == hour:
+        if row not in finalData:
+            finalData.append(row)
+            f.write(row)
+
+f.close()
+end = time.time()
+print("Elpased time (m): " + str((end - start)/60))
+
+
+
+
+
+
+
