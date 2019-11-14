baa6f7079a90c4fb615a8db67e03104ca74eeb42
Riley Kopp <koppril7@gmail.com>
2019-11-13 22:24:33 -0700

    I have created a graph!

--- timeseries/makeTensor.py
+++ timeseries/makeTensor.py
@@ -1,8 +1,24 @@
+import numpy as np
+
+
+def loadFile(filename):
+    lines = []
+    f = open(filename, 'r')
+    rawLines = f.readlines()
+    f.close()
+
+    for line in rawLines:
+        temp = line.split(",")
+        temp = (temp[0], temp[1][:-2])
+        lines.append(temp)
+
+    return set(lines)
+
 def buildGraphFromFile(filename):
-'''
-This function reads in the connections from the file and generates the graph, repeats are
-removed at load time.
-'''
+    '''
+    This function reads in the connections from the file and generates the graph, repeats are
+    removed at load time.
+    '''
 
     connections = loadFile(filename)
 
@@ -10,21 +26,43 @@ removed at load time.
 
     # format: "SourceName" : ["DestinationName", "DestinationName", ...]
     adjList = {}
+    keys = {}
+    currentKeyIdx = 0
 
 
-    # Since the graph is undirected we have to mark the connection for the src and dest
     for transmission in connections:
-        if adjList[transmission[0]]is None:
+        if adjList.get(transmission[0], None) is None:
             adjList[transmission[0]] = []
-        if adjList[transmission[1]]is None:
+            keys[transmission[0]] = currentKeyIdx
+            currentKeyIdx += 1
+        if adjList.get(transmission[1], None) is None:
             adjList[transmission[1]] = []
+            keys[transmission[1]] = currentKeyIdx
+            currentKeyIdx += 1
 
         adjList[transmission[0]].append(transmission[1])
-        adjList[transmission[1]].append(transmission[0])
-
-
-
 
     # build graph from list
+    n = len(adjList)
+    adjMatrix = np.zeros((n,n))
+
+    for row, values in adjList.items():
+        src = keys[row]
+        for col in values:
+            dest = keys[col]
+            # Since the graph is undirected we have to mark the connection for the src and dest
+            adjMatrix[src, dest] = 1
+            adjMatrix[dest, src] = 1
+
+
+
+
 
     # return graph
+    return adjMatrix, keys
+
+
+
+matrix, keys = buildGraphFromFile('../datasets/day2/day-2_hour-11.txt')
+
+print(matrix.shape)
